9f9a9972b6eafbbfa2fdcd6aaf896e7ef987d3e3
Diego Zacarias <diego.a.z.h@gmail.com>
2024-03-13 08:40:50 -0600

    chore: prettier time ts to kick deployment

--- utils/time.ts
+++ utils/time.ts
@@ -1,20 +1,20 @@
-import moment from 'moment';
+import moment from 'moment'
 
 export function getTimeUntilEvent(eventDate: Date): string {
-    const now = moment.utc();
-    const futureDate = moment(eventDate);
-    const duration = moment.duration(futureDate.diff(now));
-    const days = duration.days();
-    const hours = duration.hours();
-    const minutes = duration.minutes();
-    const seconds = duration.seconds();
-    let str = ''
-    if (days>0){
-        str = `${days} days, ${hours} hours, ${minutes} minutes`
-    } else if (hours>0){
-        str = `${hours} hours, ${minutes} minutes`
-    } else {
-        str = `${minutes} minutes, ${seconds} seconds`
-    }
-    return str;
+  const now = moment.utc()
+  const futureDate = moment(eventDate)
+  const duration = moment.duration(futureDate.diff(now))
+  const days = duration.days()
+  const hours = duration.hours()
+  const minutes = duration.minutes()
+  const seconds = duration.seconds()
+  let str = ''
+  if (days > 0) {
+    str = `${days} days, ${hours} hours, ${minutes} minutes`
+  } else if (hours > 0) {
+    str = `${hours} hours, ${minutes} minutes`
+  } else {
+    str = `${minutes} minutes, ${seconds} seconds`
+  }
+  return str
 }
